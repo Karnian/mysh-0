142e5acccfb0aa80d402a283da8e910df3698274
aeis <[khh9201@ajou.ac.kr]>
2017-10-01 10:12:49 +0900

    first work

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,6 @@
+#include <string.h>
 #include "utils.h"
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
@@ -6,4 +8,20 @@ void mysh_parse_command(const char* command,
   // TODO: Fill this!
   // HELLO!!
   // commit test
+	int count = 0;
+	char word[1000];
+	int wordidx = 0;
+	for(int i = 0; i < strlen(command); i++)
+	{
+		word[wordidx] = command[i];
+
+		if(command[i] == " ")
+		{
+			argv[count] = word;
+			count++;
+			wordidx = 0;
+		}
+	}
+	argv[count] = word;
+	argc = strlen(argv);
 }
