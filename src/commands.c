fbed5afbddf73d3812214c9cc2bfbf77a75ac9a2
Hyung Ho Kim <khh9201@ajou.ac.kr>
2017-10-09 23:56:38 +0900

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,48 +1,34 @@
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
+   if (!validate_cd_argv(argc, argv)) return -1;
 
-  // TODO: Fill it!
-	return chdir((*argv)[1]);
-  return 0;
+   // TODO: Fill it!
+   if(chdir(argv[1])) return 1;
+   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+   if (!validate_pwd_argv(argc, argv)) return -1;
 
-  // TODO: Fill it!
-	char pwdname[100000];
-	getcwd(pwdname, 100000);
-	printf("%s\n", pwdname);
-  return 0;
+   // TODO: Fill it!
+   char buf[8096];
+   getcwd(buf, 8096);
+   fprintf(stdout, "%s\n", buf);
+   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-	DIR *dp;
-	struct dirent *dirp;
-	dp = opendir(".");
-	
-	while(dirp = readdir(dp))
-	{
-		if(!strcmp(dirp->d_name, (*argv)[1]))
-			return 1;
-	}
-  return 0;
+   // TODO: Fill it!
+    if(argc==2 && strcmp(argv[0],"cd")==0) return 1;
+   return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-	for(int i = 0; i < argc; i++)
-	{
-		if(!strcmp((*argv)[i], '|'))
-			return 0;
-	}
-  return 1;
+   // TODO: Fill it!
+   if(argc == 1 && strcmp(argv[0],"pwd") == 0) return 1;
+   return 0;
 }
